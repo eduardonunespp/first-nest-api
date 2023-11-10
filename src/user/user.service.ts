ddb8096ca73d09cc53e5d242cd82b66ead14ca00
eduardonunespp <carlospn200406@gmail.com>
2023-11-09 22:21:32 -0300

    feat: add password encrypted and return users

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { createUserDto } from './dtos';
 import { User } from './interfaces';
 
@@ -7,9 +8,25 @@ export class UserService {
   private users: User[] = [];
 
   async createUser(createUserDto: createUserDto): Promise<User> {
-    return {
+    const saltOrRounds = 10;
+
+    const passwordHashed = await bcrypt.hash(
+      createUserDto.password,
+      saltOrRounds
+    );
+
+    const user: User = {
       ...createUserDto,
-      id: 1
+      id: this.users.length + 1,
+      password: passwordHashed
     };
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  async getAlluser(): Promise<User[]> {
+    return this.users;
   }
 }
